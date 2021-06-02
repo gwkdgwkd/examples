337dda464cc22f017b6afa600727e4e537c57c01
wlliang <thierryliang@126.com>
2021-06-02 20:57:03 +0800

    add clause 13,14,15,16,17
    Signed-off-by: wlliang <thierryliang@126.com>

---
+++ c++/effective_c++/clause_16.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// 成对使用new和delete时要采用相同形式
+
+class Demo {
+ public:
+  Demo() { cout << "Demo" << endl; }
+  ~Demo() { cout << "~Demo" << endl; }
+};
+
+// 使用new生成一个对象，有两件事发生：
+//  1 内存被分配出来（通过operator new函数）；
+//  2 针对此内存会有一个（或多个）构造函数被调用。
+// 使用delete，也有两件事发生：
+//  1 针对此内存会有一个（或更多）析构函数被调用;
+//  2 然后内存被释放（通过operator delete函数）。
+//  delete的最大问题在于：即将被删除的内存之内究竟有多少对象？答案决定了有多少个析构函数必须被调用起来。
+
+// 即将被删除的那个指针，所指的是单一对象或对象数组？这是必不可缺的问题，因为单一对象的内存布局一般与数组的内存布局不同。
+
+// 请记住：
+// 如果在new表达式中使用[]，必须在相应的delete表达式中也使用[]。
+// 如果在new表达式中不使用[]，一定不要在相应的delete表达式中使用[]。
+
+int main() {
+  Demo *d = new Demo[5];
+  // delete d;
+  // Demo
+  // Demo
+  // Demo
+  // Demo
+  // Demo
+  // ~Demo
+
+  delete[] d;
+  // Demo
+  // Demo
+  // Demo
+  // Demo
+  // Demo
+  // ~Demo
+  // ~Demo
+  // ~Demo
+  // ~Demo
+  // ~Demo
+
+  // 最好尽量不要对数组做typedef动作，可以使用string，vector等模板代替数组。
+  typedef string AddressLines[4];  // 使用vector<string>代替
+  string *pal = new AddressLines;
+  // delete pal;    // 行为未定义
+  delete[] pal;  // 正常
+
+  return 0;
+}
---
+++ c++/effective_c++/clause_17.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+// 以独立语句将newed对象置入智能指针
+
+class Widget {
+ public:
+  Widget() { cout << "Widget" << endl; }
+  ~Widget() { cout << "~Widget" << endl; }
+};
+int priority() {}
+void processWidget(shared_ptr<Widget> pw, int priority) {}
+
+// 请记住：
+// 以独立语句将newed对象存储于（置入）智能指针内。如果不这样做，一旦异常被抛出，有可能导致难以察觉的资源泄露。
+
+int main() {
+  // processWidget(new Widget, priority());  // 编译失败
+  // shared_ptr构造函数需要一个原始指针，但该构造函数是个explicit构造函数，无法进行隐式转换。
+
+  // 需要将new Widget的原始指针转换为processWidget所要求的shared_ptr
+  processWidget(shared_ptr<Widget>(new Widget), priority());  // 通过编译
+  // 虽然使用“对象管理式资源”，但却可能泄露资源。
+
+  // 编译器生成processWidget调用码之前，必须首先计算即将被传递的各个实参。
+  // 第二个只是一个单纯的函数调用：
+  //  调用priority；
+  // 但第一个实参（shared_ptr<Widget>(new Widget)）由两部分组成：
+  //  执行new Widget表达式；
+  //  调用shared_ptr构造函数
+  // C++编译器以什么样的次序完成这些事呢？弹性很大。（java和C#总是以特定次序完成）
+  //  可以确定的是，new Widget一定执行于shared_ptr构造函数被调用之前；
+  //  但对priority的调用可以排在1，2，3都行。
+  // 如果第2步调用并产生了异常，会发生什么事？new Widget返回的指针将会遗失，因为尚为放到shared_ptr内。
+
+  // 避免这类问题的办法很简单，使用分离语句：
+  shared_ptr<Widget> pw(new Widget);  // 在单独语句内以智能指针存储newed对象
+  processWidget(pw, priority());  // 不会造成泄露
+  // 上面的语句之所以不会泄露，是因为编译器对“跨越语句的各项操作”没有重新排列的自由（只有在语句内它才拥有那个自由度）。
+
+  return 0;
+}
