2841af8bb2071c052c79aef99be0d3517b80b9ac
wlliang <thierryliang@126.com>
2021-06-03 23:12:45 +0800

    add clause 21
    Signed-off-by: wlliang <thierryliang@126.com>

--- c++/effective_c++/clause_20.cpp
+++ c++/effective_c++/clause_20.cpp
@@ -27,7 +27,41 @@ class Student : public Person {
   string schoolName;
   string schoolAddress;
 };
-void validateStudend(Student s) {}
+void validateStudend1(Student s) {}
+void validateStudend2(const Student& s) {}
+
+class Window {
+ public:
+  string name() const { return "Window"; }
+  virtual void display() const { cout << "Window display" << endl; }
+};
+class WindowWithScroollBars : public Window {
+ public:
+  string name() const { return "WindowWithScroollBars"; }
+  virtual void display() const {
+    cout << "WindowWithScroollBars display" << endl;
+  }
+};
+
+// 以by reference方式也可以避免对象切割问题：
+//  当一个派生类对象以by value方式传递并被视为一个基类对象，基础类copy构造函数会被调用，派生类属性被切割，仅仅留下基类对象。
+void printNameAndDisplay1(Window w) {
+  // 不论传递过来的对象原本是什么类型，参数w就是一个Window对象。因此，函数内调用的是基类版本的函数。
+  std::cout << w.name() << endl;
+  w.display();
+}
+// 解决切割问题的办法，就是以by reference-to-const的方式传递w
+void printNameAndDisplay2(const Window& w) {
+  // 传进来的窗口是什么类型，w就表现出那种类型。
+  std::cout << w.name() << endl;
+  w.display();
+}
+
+// 引用往往通过指针实现，因此pass by reference通常意味着真正传递的是指针。
+// 对于内置类型，pass by vlaue往往比pass by reference的效率高些。如果可以选择，就用pass by vlaue。
+// 上面的忠告也适用于STL的迭代器和函数对象，习惯上它们都被设计为pass by vlaue。
+// 内置类型都很小，但不是小的类型就可以使用pass by vlaue。
+// 即使小型对象拥有并不昂贵的copy构造函数，还是可能有效率上的争议。编译器对待内置类型和用户自定义类型的态度截然不同，哪怕两者拥有相同的底层类型。
 
 // 请记住：
 // 尽量以pass-by-reference-to-const替换pass-by-value。前者通常比较高效，并可避免切割问题。
@@ -35,19 +69,9 @@ void validateStudend(Student s) {}
 
 int main() {
   Student p;
-  cout << "call validateStudend E:" << endl;
-  validateStudend(p);
-  cout << "call validateStudend X:" << endl;
-  // Person
-  // Student
-  // call validateStudend E:
-  // Person
-  // Student copy
-  // ~Student
-  // ~Person
-  // call validateStudend X:
-  // ~Student
-  // ~Person
+  cout << "call validateStudend1 E:" << endl;
+  validateStudend1(p);
+  cout << "call validateStudend1 X:" << endl;
 
   // validateStudend调用时，发生了什么事？
   // 无疑Student的copy构造函数会被调用，以p来初始化s。当validateStudend返回时，s会被销毁。
@@ -59,6 +83,33 @@ int main() {
   // 当函数内的Student复件被销毁，每一个构造动作都对应一个析构函数的调用动作。
   // 因此，以by value方式传递一个student对象，总体成本是“六次构造函数和六次析构函数”！
   // 这是正确且值得拥有的行为，毕竟希望所有的对象都能够被正确地构造和析构。
+
+  cout << "call validateStudend2 E:" << endl;
+  validateStudend2(p);
+  cout << "call validateStudend2 X:" << endl;
+  // 这种传递方式的效率高很多：没有任何构造函数或析构函数被调用，因为没有任何新对象被创建。
+  // const很重要，以by value方式，调用者不会修改实参，只会修改副本。以by reference方式，调用者需要考虑会不会改变实参数。
+
+  // Person
+  // Student
+  // call validateStudend1 E:
+  // Person
+  // Student copy
+  // ~Student
+  // ~Person
+  // call validateStudend1 X:
+  // call validateStudend2 E:
+  // call validateStudend2 X:
+  // ~Student
+  // ~Person
+
+  WindowWithScroollBars wwsb;
+  printNameAndDisplay1(wwsb);
+  printNameAndDisplay2(wwsb);
+  // Window
+  // Window display
+  // Window
+  // WindowWithScroollBars display
 
   return 0;
 }
---
+++ c++/effective_c++/clause_21.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// 必须返回对象时，别妄想返回其引用
+
+// 程序员一旦学习了条款20，眼中就容不下pass by value了。那样就会犯错：开始传递一些其实并不存在的对象的引用。
+
+class Rational1 {
+ public:
+  Rational1(int numerator = 0, int denominator = 1)
+      : n(numerator), d(denominator) {}
+  void display() { cout << n << "," << d << endl; }
+
+ private:
+  int n, d;
+  friend const Rational1 operator*(const Rational1& lhs, const Rational1& rhs);
+};
+
+// by value方式返回，有构造和析构的成本
+const Rational1 operator*(const Rational1& lhs, const Rational1& rhs) {
+  Rational1 result(lhs.n * rhs.n, lhs.d * rhs.d);
+  return result;
+}
+
+class Rational2 {
+ public:
+  Rational2(int numerator = 0, int denominator = 1)
+      : n(numerator), d(denominator) {}
+  void display() { cout << n << "," << d << endl; }
+
+ private:
+  int n, d;
+  friend const Rational2& operator*(const Rational2& lhs, const Rational2& rhs);
+};
+// 改为传递引用，就不需要付出代价
+// 函数创建新对象的途径有两个：在stack或heap上创建。
+#if 0
+const Rational2& operator*(const Rational2& lhs, const Rational2& rhs) {
+  // 在stack上创建新对象
+  Rational2 result(lhs.n * rhs.n, lhs.d * rhs.d);  // 返回局部变量的引用，崩溃
+  return result;
+}
+#else
+const Rational2& operator*(const Rational2& lhs, const Rational2& rhs) {
+  // 在heap内构造一个对象，必须付出一个构造函数调用的代价。
+  Rational2* result = new Rational2(lhs.n * rhs.n, lhs.d * rhs.d);  // 谁删除？
+  return *result;
+}
+#endif
+
+class Rational3 {
+ public:
+  Rational3(int numerator = 0, int denominator = 1)
+      : n(numerator), d(denominator) {}
+  // const Rational3& operator=(const Rational3& rhs) {
+  //   return Rational3(rhs.d * this->d, rhs.n * this->n);
+  // }
+  void display() { cout << n << "," << d << endl; }
+
+ private:
+  int n, d;
+  friend const Rational3& operator*(const Rational3& lhs, const Rational3& rhs);
+};
+// 还有一种方法可以避免任何构造函数被调用。
+const Rational3& operator*(const Rational3& lhs, const Rational3& rhs) {
+  // 就像所有用上static对象的设计一样，立刻会对多线程安全性产生疑虑。
+  static Rational3 result;
+  // result = lhs
+  return result;
+}
+bool operator==(const Rational3& lhs, const Rational3& rhs) { return false; }
+// Rational3 w1, x1, y1, z1;
+// if ((w1 * x1) == (y1 * z1)) {  // 总是true
+//   cout << "xiang deng" << endl;
+// } else {
+//   cout << "bu xiang deng" << endl;
+// }
+// 在==被调用前，已经有两个*被调用，每个都返回内部定义的static对象。因此==比较的都是内部的static对象，所以永远都会是true。
+
+// 一个必须返回新对象的函数的正确写法是：就让那个函数返回一个新对象。当然，需要承受返回值的构造和析构成本。
+// inline const Rational1 operator*(const Rational1& lhs, const Rational1& rhs) {
+//   return Rational1(lhs.n * rhs.n, lhs.d * rhs.d);
+// }
+// 当必须在“返回一个引用和返回一个对象”之间抉择时，选择返回对象，让编译器厂商为尽量优化吧。
+
+// 请记住：
+// 绝不要返回一个指向局部对象（local stack）的指针或引用。或返回指向heap对象，或返回指向局部静态对象的指针或引用。
+// 条款4已经为“在单线程环境中合理返回局部静态对象的引用”提供了方法。
+
+int main() {
+  Rational1 a1(1, 2);
+  Rational1 b1(3, 5);
+  Rational1 c1 = a1 * b1;
+  c1.display();  // 3,10
+
+  Rational2 a2(1, 2);
+  Rational2 b2(3, 5);
+  Rational2 c2 = a2 * b2;
+  c2.display();  // 3,10
+
+  Rational2 w, x, y, z;
+  w = x * y * z;  // 调用了两次*，使用两次new，需要两次delete
+  // 无法调用delete，绝对导致内存泄露。
+
+  return 0;
+}
