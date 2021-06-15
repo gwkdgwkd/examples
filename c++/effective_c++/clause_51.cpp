0f3af45f0716ae1e39658120b142cba98b4f142c
wlliang <thierryliang@126.com>
2021-06-15 20:53:08 +0800

    add clause 49 50 51 52 53 54 55
    Signed-off-by: wlliang <thierryliang@126.com>

---
+++ c++/effective_c++/clause_51.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+
+using namespace std;
+
+// 编写new和delete时需固守常规
+
+// 实现operator new必须返回正确的值，内存不足时必须调用new-handing函数，必须有对付零内存需求的准备，还需避免不慎掩盖正常形式的new。
+
+// operator new的返回值十分单纯。如果有能力提供内存，就返回指针指向那块内存。如果没有能力，就遵循条款49的规则，并抛出bad_alloc异常。
+// 然后其实也不是非常单纯，因为operator new实际上不只一次尝试分配内存，并在每次失败后调用new-handling函数。
+// 假设new-handling函数也许能够做某些动作将某些内存释放出来。只有当指向new-handling函数的指针是null，operator new才会抛出异常。
+
+// C++规定，即使客户要求0 byte，operator new也得返回一个合法指针。
+// void *operator new(size_t size) throw(bad_alloc) {
+//   using namespace std;
+//   if (size == 0) {
+//     size = 1;
+//   }
+//   while(true) {
+//     // 尝试分配
+//     if(分配成功)
+//       return 一个指针，指向分配的内存
+//     // 分配失败
+//     new_handler globalHandler = set_new_handler(0); // 没有办法直接取得new-handing函数指针
+//     set_new_handler(globalHandler);
+//     if(globalHandler) (*globalHandler)();
+//     else throw bad_alloc();
+//   }
+// }
+
+// 针对class设计的operator new，行为很典型地只为大小刚好sizeof(X)的对象设计。
+// 一旦被继承下去，有可能base class的operator new被调用用以分配derived class对象。
+class Base {
+ public:
+  static void* operator new(size_t size) throw(bad_alloc) {
+    // 如果大小错误，令标准的operator new处理这个size（包括size是0的情况）
+    if (size != sizeof(Base)) return ::operator new(size);
+  }
+  static void operator delete(void* rawMemory, size_t size) throw();
+};
+class Derived : public Base {};
+// 不能在Base::operator new[]内假设array的每个元素对象的大小是sizeof(Base)，这意味着不能假设array元素对象个数为：byte申请数/sizeof(Base)。
+// 此外，传递给operator new[]的size_t参数，其值可能比“将被填充以对象”的内存数量更多。因为动态分配的array可能包含额外空间来存放元素个数。
+
+// operator delete情况更简单，需要记住的唯一事情就是C++保证“删除null指针永远安全”,所以必须实现这项保证：
+void operator delete(void* rawMemory) throw() {
+  if (rawMemory == 0) return;
+  // ...
+}
+// 这个函数的member版本也很简单，只需要多加一个动作检查删除数量。
+void Base::operator delete(void* rawMemory, size_t size) throw() {
+  if (rawMemory == 0) return;
+  if (size != sizeof(Base)) {
+    ::operator delete(rawMemory);
+    return;
+  }
+  // 归还rawMemory所指的内存
+  return;
+}
+
+// 如果即将被删除的对象派生自某个base class而后者欠缺virtual析构函数，那么C++传给operator delete的size数值可能不正确。
+// 这是“让你的base class拥有virtual析构函数”的一个够好的理由。
+// 换句话说，如果base class遗漏virtual析构函数，operator delete可能无法正确运作。
+
+// 请记住：
+// operator new应该内含一个无穷循环，并在其中尝试分配内存，如果它无法满足内存需求，就应该调用new_handler。
+// 它也应该有能力处理0 byte申请。class专属版本则还应处理“比正确大小更大的（错误）申请”。
+// operator delete应该在收到null指针时不做任何事情。class专属版本则还应该处理“比正确大小更大的（错误）申请”。
+
+int main() {
+  Derived* p = new Derived;  // 调用的是Base::operator new
+
+  return 0;
+}
---
+++ c++/effective_c++/clause_52.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+
+using namespace std;
+
+// 写了placement new也要写placement delete
+
+class Widget1 {};
+
+// 写了一个class专属的operator new，要求接受一个ostream，用来标记相关分配信息，同时有写了一个正常形式的class专属operator delete：
+class Widget {
+ public:
+  // 非正常形式的new，这便是所谓的placement new。
+  static void* operator new(size_t size, ostream& logStream) throw(bad_alloc) {}
+  // 正常的class专属delete
+  static void operator delete(void* pMemory, size_t size) throw() {}
+};
+// placement new意味带任意额外参数的new，placement delete直接派生自它。
+// 当new调用成功，default构造函数抛出异常时，C++运行系统无法知道真正被调用的那个new如何运作，也就无法取消分配并恢复原样(没有任何delete被调用)。
+// 运行期系统寻找”参数个数和类型都与operator new相同“的某个delete。如果找到就调用。所以delete应该是：
+// void operator delete(void*,ostream&) throw();
+class Widget2 {
+ public:
+  static void* operator new(size_t size, ostream& logStream) throw(bad_alloc) {}
+  static void operator delete(void* pMemory, ostream& logStream) throw() {}
+  // placement delete只有在“伴随placement new调用而触发的构造函数”出现异常时才会被调用。
+  // 对着指针执行delete绝不会导致调用placement delete。
+  // 所以必须同时placement delete（构造函数抛出异常）和正常的operator delete（没有异常）两个版本。
+  static void operator delete(void* pMemory) throw() {}  // 正常delete版本
+};
+
+// 由于成员函数的名称会掩盖其外围作用域中的相同名称，必须小心避免让class专属的new掩盖客户期望的其他new（包括正常版本）。
+// 假设有一个base class，声明唯一一个placement operator new，客户端会发现无法使用正常形式的new：
+class Base {
+ public:
+  static void* operator new(size_t size, ostream& logStream) throw(bad_alloc) {}
+};
+// 同样的道理，derived class中的operator new会掩盖global版本和继承而得的operator new版本：
+class Derived : public Base {
+ public:
+  static void* operator new(size_t size) throw(bad_alloc) {}
+};
+
+// 缺省情况下C++在global作用域内提供以下形式的operator new：
+// void* operator new(size_t) throw(bad_alloc);           // normal new
+// void* operator new(size_t, void*) throw();             // placement new
+// void* operator new(size_t, const nothrow_t&) throw();  // nothrow new
+// 在class内声明任何operator new，都会遮掩上述的这些标准形式。
+// 如果希望这些函数有着平常的行为，只要令你的class专属版本调用global版本即可。
+
+// 简单的做法是，建立一个base class，内含所有正常形式的new和delete：
+class StandardNewDeleteForms {
+ public:
+  // normal new/delete
+  static void* operator new(size_t size) throw(bad_alloc) {
+    return ::operator new(size);
+  }
+  static void operator delete(void* pMemory) throw() {
+    ::operator delete(pMemory);
+  }
+  // placement new/delete
+  static void* operator new(size_t size, void* ptr) throw() {
+    ::operator new(size, ptr);
+  }
+  static void operator delete(void* pMemory, void* ptr) throw() {
+    ::operator delete(pMemory, ptr);
+  }
+  // nothrow new/delete
+  static void* operator new(size_t size, const nothrow_t& nt) throw() {
+    return ::operator new(size, nt);
+  }
+  static void operator delete(void* pMemory, const nothrow_t&)throw() {
+    ::operator delete(pMemory);
+  }
+};
+// 凡是想扩充标准形式的客户，可以利用继承机制及using声明获取标准形式：
+class Widget3 : public StandardNewDeleteForms {
+  using StandardNewDeleteForms::operator new;
+  using StandardNewDeleteForms::operator delete;
+  static void* operator new(size_t, ostream& logStream) throw(bad_alloc) {}
+  static void operator delete(void* pMemory, ostream& logStream) throw() {}
+};
+
+// 请记住：
+// 当写一个placement operator new，请确定也出了对应的placement operator delete。否则程序可能发生隐秘的时断时续的内存泄露。
+// 当声明placement new和placement delete，请确定不要无意识（非故意）地遮掩了它们的正常版本。
+
+int main() {
+  Widget1* pw = new Widget1;
+  // 共有两个函数被调用，一个是operator new，一个是Widget的default构造函数。
+  // 假设new调用成功，default构造函数抛出异常。那么new分配的内存必须取消并恢复原样，否则会造成内存泄露。
+  // 这个时候客户无法归还内存，因为pw尚未被赋值，客户手上也就没有指针指向未被归还的内存。取消步骤需要C++运行期系统身上。
+  // 运行期系统会高高兴兴地调用new对应的delete版本，前提当然是它必须知道哪一个delete该被调用。
+
+  Widget* pw1 = new (cerr) Widget;  // 构造函数发生异常，还会发生内存泄露
+
+  Widget2* pw2 = new (cerr) Widget2;  // 构造函数发生异常，不会在泄露内存
+  delete pw2;                         // 调用正常的operator delete
+
+  Base* pb2 = new (cerr) Base;  // 正确，调用Base的placement new
+  // Base* pb1 = new Base;  // 错误，正常形式的operator new被掩盖
+
+  Derived* pd1 = new Derived;
+  // Derived* pd2 = new (clog) Derived;  // 错误，Base的placement new被掩盖了
+
+  return 0;
+}
