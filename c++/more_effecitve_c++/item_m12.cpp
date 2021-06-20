f4a5d50b8c1bc42d0b11d824aded04504f4823b7
wlliang <thierryliang@126.com>
2021-06-20 22:07:44 +0800

    add item_m 12
    Signed-off-by: wlliang <thierryliang@126.com>

--- c++/more_effecitve_c++/item_m12.cpp
+++ c++/more_effecitve_c++/item_m12.cpp
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <iostream>
 
 using namespace std;
@@ -55,5 +56,100 @@ void passAndThrowWidget2() {
   // 这种行为可能与你所期待的不一样，但是这与在其他情况下C++中拷贝构造函数的行为是一致的。
   throw rw;
 }
+
+// 异常是其它对象的拷贝,这个事实影响到你如何在catch块中再抛出一个异常。
+// catch (Widget& w) { // 捕获Widget异常
+//   ...     // 处理异常
+//   throw;  // 重新抛出异常,让它继续传递
+// }
+// catch (Widget& w) {
+//   ...       // 处理异常
+//   throw w;  // 传递被捕获异常的拷贝
+// }
+// 这两个catch块的差别在于第一个catch块中重新抛出的是当前捕获的异常,而第二个catch块中重新抛出的是当前捕获异常的一个新的拷贝。
+// 如果忽略生成额外拷贝的系统开销,这两种方法还有差异么?当然有。
+//  第一个块中重新抛出的是当前异常(current exception),无论它是什么类型。
+//  特别是如果这个异常开始就是做为SpecialWidget类型抛出的,那么第一个块中传递出去的还是SpecialWidget异常,即使w的静态类型(static type)是Widget。
+//  这是因为重新抛出异常时没有进行拷贝操作。
+//  第二个catch块重新抛出的是新异常,类型总是Widget,因为w的静态类型(static type)是Widget。
+//  一般来说,你应该用throw来重新抛出当前的异常,因为这样不会改变被传递出去的异常类型,而且更有效率,因为不用生成一个新拷贝。
+
+// catch (Widget w)  // 通过传值捕获异常
+// catch (Widget& w) // 通过传递引用捕获异常
+// catch (const Widget& w) //通过传递指向const的引用捕获异常
+// 传递参数与传递异常的另一个差异:
+//  一个被异常抛出的对象(总是一个临时对象)可以通过普通的引用捕获;它不需要通过指向const对象的引用(reference-to-const)捕获。
+//  在函数调用中不允许转递一个临时对象到一个非const引用类型的参数里,但是在异常中却被允许。
+
+// 当用传值的方式传递函数的参数,我们制造了被传递对象的一个拷贝,并把这个拷贝存储到函数的参数里。同样我们通过传值的方式传递一个异常时,也是这么做的。
+// catch (Widget w) // 通过传值捕，会建立两个被抛出对象的拷贝,一个是所有异常都必须建立的临时对象,第二个是把临时对象拷贝进w中。
+// catch (Widget& w) // 通过引用捕获，仍旧会建立一个被抛出对象的拷贝:拷贝同样是一个临时对象。相反当我们通过引用传递函数参数时,没有进行对象拷贝。
+// 当抛出一个异常时,系统构造的(以后会析构掉)被抛出对象的拷贝数比以相同对象做为参数传递给函数时构造的拷贝数要多一个。
+
+// 通过指针抛出异常与通过指针传递参数是相同的。不论哪种方法都是一个指针的拷贝被传递。
+// 但你不能认为抛出的指针是一个指向局部对象的指针,因为当异常离开局部变量的生存空间时,该局部变量已经被释放。
+// catch子句将获得一个指向已经不存在的对象的指针。这种行为在设计时应该予以避免。也就是说:必须是全局的或堆中的。
+
+// 对象从函数的调用处传递到函数参数里与从异常抛出点传递到catch子句里所采用的方法不同,这只是参数传递与异常传递的区别的一个方面;
+// 第二个差异是在函数调用者或抛出异常者与被调用者或异常捕获者之间的类型匹配的过程不同。
+// 一般来说,catch子句匹配异常类型时不会进行这样的转换。
+bool someFunction() {
+  int i;
+  double sqrtOfi = sqrt(i);  // C++允许进行从int到double的隐式类型转换
+  return true;
+}
+void f(int value) {
+  try {
+    if (someFunction()) {
+      throw value;  // 如果someFunction()返回真,抛出一个整形值
+    }
+  } catch (double d) {
+    // 只处理double类型的异常
+    // 但在try块中抛出的int异常不会被处理double异常的catch子句捕获。
+    // 该子句只能捕获类型真真正正为double的异常,不进行类型转换。因此如果要想捕获int异常,必须使用带有int或int&参数的catch子句。
+  }
+}
+
+// 不过在catch子句中进行异常匹配时可以进行两种类型转换:
+// 第一种是继承类与基类间的转换。一个用来捕获基类的catch子句也可以处理派生类类型的异常。
+// 例如在标准C++库(STL)定义的异常类层次中的诊断部分(diagnostics portion)。
+// 捕获runtime_errors异常的Catch子句可以捕获range_error类型和overflow_error类型的异常;
+// 可以接收根类exception异常的catch子句能捕获其任意派生类异常。
+// 这种派生类与基类(inheritance_based)间的异常类型转换可以作用于数值、引用以及指针上:
+// can catch errors of type runtime_error，range_error or overflow_error：
+// catch (runtime_error)
+// catch (runtime_error&)
+// catch (const runtime_error&)
+// can catch errors of type runtime_error* range_error*, or overflow_error*
+// catch (runtime_error*)
+// catch (const runtime_error*)
+// 第二种是允许从一个类型化指针(typed pointer)转变成无类型指针(untyped pointer),所以带有const void*指针的catch子句能捕获任何类型的指针类型异常:
+// catch (const void*) // 捕获任何指针类型异常
+
+// 传递参数和传递异常间最后一点差别是catch子句匹配顺序总是取决于它们在程序中出现的顺序。
+// 因此一个派生类异常可能被处理其基类异常的catch子句捕获,即使同时存在有能直接处理该派生类异常的catch子句,与相同的try块相对应。
+// try {
+// } catch (logic_error& ex) {
+//   // 这个catch块将捕获所有的logic_error异常, 包括它的派生类
+// } catch (invalid_argument& ex) {
+//   // 这个块永远不会被执行，因为所有的invalid_argument异常都被上面的catch子句捕获。
+// }
+// 如果一个处理派生类异常的catch子句位于处理基类异常的catch子句后面,编译器会发出警告。因为这样的代码在 ++里通常是不合法的。
+// 不过你最好做好预先防范:不要把处理基类异常的catch子句放在处理派生类异常的catch子句的前面。应该写成这样：
+// try {
+// } catch (invalid_argument& ex {
+//   // 处理invalid_argument异常
+// } catch (logic_error& ex) {
+//   // 处理所有其它的logic_errors异常
+// }
+
+// 与上面这种行为相反,当你调用一个虚拟函数时,被调用的函数位于与发出函数调用的对象的动态类型(dynamic type)最相近的类里。
+// 你可以这样说虚拟函数采用最优适合法,而异常处理采用的是最先适合法。
+
+// 综上所述,把一个对象传递给函数或一个对象调用虚拟函数与把一个对象做为异常抛出,这之间有三个主要区别：
+//  第一、异常对象在传递时总被进行拷贝;当通过传值方式捕获时,异常对象被拷贝了两次。对象做为参数传递给函数时不一定需要被拷贝。
+//  第二、对象做为异常被抛出与做为参数传递给函数相比,前者类型转换比后者要少(前者只有两种转换形式)。
+//  最后一点,catch子句进行异常类型匹配的顺序是它们在源代码中出现的顺序,第一个类型匹配成功的catch将被用来执行。
+//          当一个对象调用一个虚拟函数时,被选择的函数位于与对象类型匹配最佳的类里,即使该类不是在源代码的最前头。
 
 int main() { return 0; }
