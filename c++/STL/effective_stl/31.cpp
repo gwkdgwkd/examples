336187d4a96f0ba220daac6e3c5c7d866ba8712b
wlliang <thierryliang@126.com>
2021-07-07 21:04:46 +0800

    add effective_stl 30 31 32
    Signed-off-by: wlliang <thierryliang@126.com>

---
+++ c++/STL/effective_stl/31.cpp
@@ -0,0 +1,98 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+using namespace std;
+
+// 了解各种与排序有关的选择
+
+class Widget {};
+bool qualityCompare(const Widget &lhs, const Widget &rhs) {}
+// sort是一个非常不错的算法，但也并非在任何场合下都是完美无缺的。
+// 只需排序出前20好的，可以使用partial_sort。
+// 选出最好的几个，可以使用nth_element。
+// partial_sort和nth_element效果基本相同，不同的是partial_sort排序了，nth_element没有排序。
+
+// 对于等价的元素，partial_sort和nth_element有自己的做法，无法控制它们的行为。
+// partial_sort、nth_element和sort都属于非稳定的排序算法，stable_sort可以提供稳定排序。partial_sort和nth_element没有对应的稳定排序。
+
+// partition算法可以把满足某个特定条件的元素放在区间的前部。如果相对位置非常重要，那么可以使用stable_partition。
+bool hasAcceptableQuality(const Widget &w) {}
+
+// partial_sort、nth_element、sort和stable_sort算法都要求随机访问迭代器，所以这些算法只能被用于vector、string、deque和数组。
+// 对于关联容器进行排序并没有实际意义。list是唯一需要排序却无法使用这些排序算法的容器。为此，list特别提供了sort成员函数（稳定排序）。
+// 如果需要在list中使用partial_sort、nth_element，只能使用间接的方法：
+//  将list中的元素拷贝到一个提供随机访问的迭代器容器中，然后对该容器执行期望的算法；
+//  先创建一个list::iterator的容器，在对容器执行相应的算法，然后通过其中的迭代器访问list的元素；
+//  利用一个包含迭代器的有序容器中的信息，通过反复地调用splice成员函数，将list中的元素跳转到期望的目标位置。
+
+// 与partial_sort、nth_element、sort和stable_sort不同的是，partition和stable_partition只要求双向迭代器就能完成工作。
+// 所以对标准序列容器，都可以使用partition和stable_partition。
+
+// 总结：
+// 如果需要对vector、string、deque或数组进行完全排序，那么可以使用sort或者stable_sort。
+// 如果有一个vector、string、deque或数组，并且只需要对等价性最前面的n个元素进行排序，那么可以使用partial_sort。
+// 如果有一个vector、string、deque或数组，并且需要找到第n个位置上的元素，或者需要找到等价性最前面的n个元素但有不必对这n个元素进行排序，那使用nth_element。
+// 如果需要将一个标准序列容器中的元素按照是否满足某个特定的条件区分开来，那么partition和stable_partition可以做到。
+// 如果数据在list中，可以直接调用partition和stable_partition，可以用list::sort来代替sort和stable_sort算法。partial_sort、nth_element功能需要间接实现。
+// 可以通过使用关联容器来保证元素始终保存特定的顺序。也可以考虑使用标准的非STL容器priority_queue，它总是保持其元素的顺序关系。
+
+// 稳定的排序算法要比那些忽略稳定性的算法更为耗时。按消耗资源排序，较少的算法排在前面：
+// partition、stable_partition、nth_element、partial_sort、sort、stable_sort
+
+int main() {
+  vector<Widget> widgets;
+  partial_sort(widgets.begin(), widgets.begin() + 20, widgets.end(),
+               qualityCompare);
+
+  nth_element(widgets.begin(), widgets.begin() + 19, widgets.end(),
+              qualityCompare);
+
+  vector<int> v{9, 4, 8, 2, 7, 3, 6, 1, 5};
+  copy(v.begin(), v.end(), ostream_iterator<int>(cout, " "));
+  cout << endl;  // 9 4 8 2 7 3 6 1 5
+  partial_sort(v.begin(), v.begin() + 5, v.end());
+  copy(v.begin(), v.end(), ostream_iterator<int>(cout, " "));
+  cout << endl;  // 1 2 3 4 5 9 8 7 6
+
+  vector<int> v1{9, 4, 8, 2, 7, 3, 6, 1, 5};
+  copy(v1.begin(), v1.end(), ostream_iterator<int>(cout, " "));
+  cout << endl;  // 9 4 8 2 7 3 6 1 5
+  nth_element(v1.begin(), v1.begin() + 5, v1.end());
+  copy(v1.begin(), v1.end(), ostream_iterator<int>(cout, " "));
+  cout << endl;  // 5 4 1 2 3 6 7 8 9
+
+  // nth_element除了可以找出排名前n个元素，还有其他功能，比如可以用来找到一个区间的中间值，或者找到某个特定百分比上的值：
+  vector<Widget>::iterator begin(widgets.begin());
+  vector<Widget>::iterator end(widgets.end());
+  // 找出具有中间质量级别的Widget
+  vector<Widget>::iterator goalPosition;
+  goalPosition = begin + widgets.size() / 2;
+  nth_element(begin, goalPosition, end, qualityCompare);
+  // 找出具有75%质量的元素
+  vector<Widget>::size_type goalOffset = 0.25 * widgets.size();
+  nth_element(begin, begin + goalOffset, end, qualityCompare);
+
+  vector<int> v2{9, 4, 8, 2, 7, 3, 6, 1, 5};
+  vector<int>::iterator begin1(v2.begin());
+  vector<int>::iterator end1(v2.end());
+  vector<int>::iterator goalPosition1;
+  goalPosition1 = begin1 + v2.size() / 2;
+  nth_element(begin1, goalPosition1, end1);
+  copy(v1.begin(), v1.end(), ostream_iterator<int>(cout, " "));
+  cout << endl;  // 5 4 1 2 3 6 7 8 9
+
+  vector<int> v3{9, 4, 8, 2, 7, 3, 6, 1, 5};
+  vector<int>::iterator begin2(v3.begin());
+  vector<int>::iterator end2(v3.end());
+  vector<int>::size_type goalOffset2 = 0.25 * v3.size();
+  nth_element(begin2, begin2 + goalOffset2, end2);
+  copy(v3.begin(), v3.end(), ostream_iterator<int>(cout, " "));
+  cout << endl;  // 1 2 3 4 5 7 6 8 9
+
+  // vector<Widget>::iterator goodEnd = partition(widgets.begin(), widgets.end(), hasAcceptableQuality);
+  // 调用partition后，所有满足要求的都被放在了begin和goodEnd之间的区间中。其他大在goodEnd到end之间的区间中。
+
+  return 0;
+}
---
+++ c++/STL/effective_stl/32.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <iostream>
+#include <list>
+#include <vector>
+
+using namespace std;
+
+// 如果确实需要删除元素，则需要在remove这一类算法之后调用erase
+
+// remove是STL中最令人感到疑惑的算法。
+// remove也需要一对迭代器来指定所要进行操作的元素区间。它并不接受容器作为参数，所以remove并不知道这些元素被存放在哪个容器中。
+// 并且remove也不可能推断出是什么容器，因为无法从迭代器推知对应的容器类型。
+// 唯一的办法是调用容器的成员函数，几乎总是erase的某种形式。
+// list有几个可以删除元素的成员函数，但它们没有被命名为erase，不过它们确实是成员函数。
+// 所以remove不可能从容器中删除元素（不知道是list，list的名称不是erase）。
+// 这也说明一个现象：用remove从容器删除元素，而容器中的元素数目却不会因此而减少。remove不是真正意义上的删除，因为它做不到。
+
+// remove移动了区间中的元素，其结果是，“不用被删除”的元素移到了区间前部（保持原来的相对顺序）。
+// 它返回一个迭代器指向最后一个不用被删除的元素之后的元素。相当于该区间新的逻辑结尾。
+// 通常来说，当调用了remove后，从区间中被删除的那些元素可能在也可能不在区间中。
+// 如果真想删除元素，那就必须在remove之后使用erase。
+// 把remove返回的迭代器作为区间形式erase的第一个实参是很常见的，这是个习惯用法。
+// remove和erase配合的如此紧密，以至于它们被合并起来融入到了list的remove成员函数中。这是STL中唯一一个名remove并且确实删除了元素的函数。
+// list的remove也是不一致的地方，在关联容器中类似的函数被称为erase。照理来说，list的remove也应该被称为erase。
+
+// remove并不是唯一一个适用这种情形的算法，其他还有两个属于remove类的算法：remove_if和unique。
+// remove_if和remove的相似性是很显然的。但是unique也和remove行为相似。unique也需要在没有任何容器信息的情况下，
+// 从容器中删除一些元素（相邻的、重复的值）。所以如果真想从容器中删除元素的话，就必须在调用unique之后在调用erase。
+// unique与list的结合也与remove的情形类似。如同list::remove会真正删除元素一样，list::unique也会真正删除元素。
+
+int main() {
+  vector<int> v;
+  v.reserve(10);
+  for (int i = 1; i <= 10; ++i) {
+    v.push_back(i);
+  }
+  cout << v.size() << endl;  // 10
+  v[3] = v[5] = v[9] = 99;
+  remove(v.begin(), v.end(), 99);
+  cout << v.size() << endl;  // 10
+
+  vector<int> v1;
+  v1.reserve(10);
+  for (int i = 1; i <= 10; ++i) {
+    v1.push_back(i);
+  }
+  cout << v1.size() << endl;  // 10
+  v1[3] = v1[5] = v1[9] = 99;
+  v1.erase(remove(v1.begin(), v1.end(), 99), v1.end());
+  cout << v1.size() << endl;  // 7
+
+  list<int> li{1, 2, 99, 3, 4, 5, 6, 99, 88, 99};
+  cout << li.size() << endl;  // 10
+  li.remove(99);
+  cout << li.size() << endl;  // 7
+
+  return 0;
+}
