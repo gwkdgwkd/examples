f7660c33e29c4661e6b6e37a0d5ffdab98fe2964
wlliang <thierryliang@126.com>
2021-07-16 21:31:14 +0800

    git add effective_stl 38 39 40
    Signed-off-by: wlliang <thierryliang@126.com>

---
+++ c++/STL/effective_stl/39.cpp
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+using namespace std;
+
+// 确保判别式是“纯函数”
+
+// 一个判别式（predicate）是一个返回值为bool类型（或者可以隐式地转换为bool类型）的函数。在STL中，判别式有着广泛的用途。
+// 标准关联容器的比较函数就是判别式；对于像find_if以及各种与排序有关的算法，判别式往往也被作为参数来传递。
+
+// 一个纯函数是指返回值仅仅依赖于其参数的函数。
+// 在C++中，纯函数所能访问的数据应该局限于参数以及常量（在函数生命期内不会被改变，应该被声明为const）。如果一个纯函数需要访问那些
+// 可能会在两次调用之间发生变化的数据，那么相同的参数在不同的时刻调用该函数就有可能得到不同的结果，与纯函数的定义相矛盾。
+
+// 判别式类是一个函数子类，它的operator()函数是一个判别式，也就是说，operator()返回true或者flase。
+// STL中凡是能接受判别式的地方，就既可以接受一个真正的判别式，也可以接受一个判别式类的对象。
+
+// 接受函数子的STL算法可能会先创建函数子的拷贝，然后存放起来待以后再使用这些拷贝，而且，有些STL算法实现也确实利用了这一特性。
+// 而这一特性的直接反映就是：要求判别式函数必须是纯函数。
+class BadPredicate : public unary_function<int, bool> {
+ public:
+  BadPredicate() : timesCalled(0) {}
+  bool operator()(const int&) { return ++timesCalled == 3; }
+  // 解决下面问题的办法是把operator()函数声明为const，这样编译器不会让你改变任何一个成员：
+  // bool operator()(const int&) const { return ++timesCalled == 3; }  // 错误
+
+ private:
+  size_t timesCalled;
+};
+
+// 将判别式的operator()函数声明为const，还远远不够。
+// 即使是const成员函数，它也可以访问mutable数据成员、非cosnt对象、以及非const的全局对象。
+// 一个精心设计的判别式应该保证其operator()函数完全独立所有这些变量。
+// 一个行为正常的判别式的operator()肯定是const的，还应该是一个“纯函数”。
+// STL中的判别式函数和判别式类的对象可以互相代替，所以判别式函数也应该和判别式类中的operator()函数一样，应该是纯函数：
+bool anotherBadPredicate(const int&, const int&) {
+  static int timesCalled = 0;  // 切记不可如此
+  return ++timesCalled == 3;   // 应该是纯函数，而纯函数没有状态
+}
+
+int main() {
+  vector<int> vw{1, 2, 3, 4, 5, 6};
+  copy(vw.begin(), vw.end(), ostream_iterator<int>(cout, " "));
+  cout << endl;  // 1 2 3 4 5 6
+  vw.erase(remove_if(vw.begin(), vw.end(), BadPredicate()), vw.end());
+  copy(vw.begin(), vw.end(), ostream_iterator<int>(cout, " "));
+  cout << endl;  // 1 2 4 5
+  // 不仅删除了第三个，而且还删除了第六个
+  // remove_if中调用了find_if和remove_copy_if，在find_if中加到了3次，然后拷贝给remove_copy_if，调用三次后还返回true。
+
+  return 0;
+}
---
+++ c++/STL/effective_stl/40.cpp
@@ -0,0 +1,87 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <list>
+
+using namespace std;
+
+// 若一个类是函数子，则应使它可配接
+
+struct Widget {};
+bool isInteresting(const Widget* pw) {}
+
+// STL的4个标准的函数配接器（not1、not2、bind1st和bind2nd）都要求一些特殊的类型定义，那些非标准的、与STL兼容的配接器通常也是如此。
+// 提供了这些必要的类型定义的函数对象被称为可配接的函数对象，反之，如果函数对象缺少这些类型定义，则称为不可配接的。
+// 可配接的函数对象能够与其他STL组件更为默契地协同工作，它们能够应用于更多的上下文环境中，因此应当尽可能地使你编写的函数对象可以配接。
+// 这些类型是:argument_type、first_argument_type、second_argument_type已经result_type。
+// 不同种类的函数子类所提供的定义也不尽相同，是上面类型的不同子集。提供这些类型定义最简单的办法是让韩淑子从特定的基类继承：
+//  如果函数子类的operator()只有一个实参，那么它应该从std::unary_function继承；
+//  如果函数子类的operator()有两个实参，那么它应该从std::binary_function继承。
+// std::unary_function和std::binary_function是模板，不能直接继承，必须继承它们所产生的结构，就是要指定某些类型实参。
+// 对于std::unary_function必须指定函数子类operator()所带的参数的类型，以及返回类型。
+// 而对于std::binary_function必须指定三个类型，operator()的第一个和第二个参数的类型，以及operator()的返回类型。
+// std::unary_function和std::binary_function operator()的返回类型是std::unary_function和std::binary_function的最后一个参数。
+template <typename T>
+class MeetsThreshold : public std::unary_function<Widget, bool> {
+ private:
+  // 使用class是因为包含了状态信息，数据成员
+  const T threshold;
+
+ public:
+  MeetsThreshold(const T& t) : threshold(t) {}
+  bool operator()(const Widget&) const {}
+};
+struct WidgetNameCompare : public std::binary_function<Widget, Widget, bool> {
+  // 不包含状态信息，因而不需要任何私有成员。
+  // 如果一个函数子的所有成员都是公有的，那么通常会将其声明为结构而不是类。
+  // STL中所有无状态的函数子类一般都被定义成结构。
+  bool operator()(const Widget& lhs, const Widget& rhs) const {}
+};
+// 虽然operator()的参数类型都是const Widget&,但我们传递给binary_function的类型却是Widget。
+// 一般情况下，传递给std::unary_function和std::binary_function的非指针类型需要去掉const和引用(&)部分。
+
+// 如果operator()带有指针参数，规则又不同了：
+struct PtrWidgetNameCompare
+    : public std::binary_function<const Widget*, const Widget*, bool> {
+  bool operator()(const Widget* lhs, const Widget* rhs) const {}
+};
+// 对于以指针作为参数或返回类型的函数子类，一般的规则是，传给std::unary_function和std::binary_function的类型
+// 与operator()的参数和返回类型完全相同。
+
+// STL函数对象是C++函数的一种抽象和建摸形式，而每个C++函数只有一组确定的参数类型和一个返回类型。
+// 所以STL总是假设每个函数子类只有一个operator()成员函数，并且其参数和返回类型应该吻合std::unary_function和std::binary_function的模板参数。
+// 这也就意味着，虽然只要创建一个包含两个operator函数的结构，就可以把WidgetNameCompare和PtrWidgetNameCompare的功能合并到
+// 一个函数子结构中，但最好不要这样做。如果这样做了，那么这样的函数子至多只有一种调用形式是可配接的。
+// 一个只有一半配接能力的函数子恐怕并不比完全不可配接的函数子强多少。
+// 但有时确实需要函数子类具有多种不同的调用形式，也就意味这放弃了其可配接能力。
+
+int main() {
+  list<Widget*> widgetPtrs;
+
+  // 找到第一个满足条件的
+  list<Widget*>::iterator i =
+      find_if(widgetPtrs.begin(), widgetPtrs.end(), isInteresting);
+  if (i != widgetPtrs.end()) {
+  }
+
+  // 找到第一个不满足条件的
+  // find_if(widgetPtrs.begin(), widgetPtrs.end(), not1(isInteresting));  // 编译失败
+  // isInteresting作为一个基本的函数指针，它缺少not1所需要的类型定义。
+
+  // 正确的做法是：
+  find_if(widgetPtrs.begin(), widgetPtrs.end(), not1(ptr_fun(isInteresting)));
+  // ptr_fun只是完成了一些类型定义而已，这些类型定义是not1所必须的。
+
+  list<Widget> widgets;
+  Widget a;
+  // 找到最后一个不符合的
+  list<Widget>::reverse_iterator i1 =
+      find_if(widgets.rbegin(), widgets.rend(), not1(MeetsThreshold<int>(10)));
+  // 找到按WidgetNameCompare定义的规则排序时，在a之前的第一个Widget对象
+  list<Widget>::reverse_iterator i2 = find_if(widgets.rbegin(), widgets.rend(),
+                                              bind2nd(WidgetNameCompare(), a));
+  // 如果MeetsThreshold和WidgetNameCompare不是从std::unary_function和std::binary_function继承而来的，
+  // 上面的例子就无法通过编译，因为not1和bind2nd只能用于可配接的函数对象。
+
+  return 0;
+}
