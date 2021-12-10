892a606de5882491a37005af226159e36c6a020c
wlliang <thierryliang@126.com>
2021-12-10 11:58:39 +0800

    add builder, factory, abstract_factory, prototype
    Signed-off-by: wlliang <thierryliang@126.com>

---
+++ design_pattern/c++/对象创建模式/abstract_factory/good/EmployeeDAO1.cpp
@@ -0,0 +1,56 @@
+
+// 工厂模式，但有问题:IDBConnection,IDBCommand,IDataReader可能不同
+
+// 数据库访问有关的基类
+class IDBConnection {};
+class IDBConnectionFactory {
+ public:
+  virtual IDBConnection* CreateDBConnection() = 0;
+};
+
+class IDBCommand {};
+class IDBCommandFactory {
+ public:
+  virtual IDBCommand* CreateDBCommand() = 0;
+};
+
+class IDataReader {};
+class IDataReaderFactory {
+ public:
+  virtual IDataReader* CreateDataReader() = 0;
+};
+
+// 支持SQL Server
+class SqlConnection : public IDBConnection {};
+class SqlConnectionFactory : public IDBConnectionFactory {};
+
+class SqlCommand : public IDBCommand {};
+class SqlCommandFactory : public IDBCommandFactory {};
+
+class SqlDataReader : public IDataReader {};
+class SqlDataReaderFactory : public IDataReaderFactory {};
+
+// 支持Oracle
+class OracleConnection : public IDBConnection {};
+class OracleCommand : public IDBCommand {};
+class OracleDataReader : public IDataReader {};
+class EmployeeDAO {
+  IDBConnectionFactory* dbConnectionFactory;
+  IDBCommandFactory* dbCommandFactory;
+  IDataReaderFactory* dataReaderFactory;
+
+ public:
+  vector<EmployeeDO> GetEmployees() {
+    IDBConnection* connection = dbConnectionFactory->CreateDBConnection();
+    connection->ConnectionString("...");
+
+    IDBCommand* command = dbCommandFactory->CreateDBCommand();
+    command->CommandText("...");
+    command->SetConnection(connection);  // 关联性
+
+    IDBDataReader* reader = dataReaderFactory->CreateDataReader();
+    reader->ExecuteReader();  // 关联性
+    while (reader->Read()) {
+    }
+  }
+};
