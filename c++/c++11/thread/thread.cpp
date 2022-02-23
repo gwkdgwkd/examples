c03d80276640b53f4843babd926e0045cd7381e9
wlliang <thierryliang@126.com>
2022-02-23 14:25:36 +0800

    update c++11 thread
    Signed-off-by: wlliang <thierryliang@126.com>

--- c++/c++11/thread/thread.cpp
+++ c++/c++11/thread/thread.cpp
@@ -1,16 +1,12 @@
 #include <pthread.h>
 #include <stdarg.h>
-#include <cassert>
 #include <cstring>
-#include <ctime>    // std::time_t, std::tm, std::localtime, std::mktime
+#include <ctime>  // std::time_t, std::tm, std::localtime, std::mktime
+#include <functional>
 #include <iomanip>  // std::put_time
 #include <iostream>
 #include <mutex>
 #include <thread>
-
-// https://www.freesion.com/article/32321338069/
-
-using namespace std;
 
 namespace create {
 // 只要创建了线程对象，线程就开始执行。并没有一个类似start的函数来显式启动。
@@ -43,9 +39,9 @@ void testCreate() {
 
   // 移动拷贝构造函数
   std::thread t5(func);
-  cout << "t5 id is " << t5.get_id() << endl;  // 139858793432832
+  std::cout << "t5 id is " << t5.get_id() << std::endl;  // 139858793432832
   std::thread t6 = move(t5);  // t6获t5的全部属性，t5被消耗了
-  cout << "t6 id is " << t6.get_id() << endl;  // 139858793432832
+  std::cout << "t6 id is " << t6.get_id() << std::endl;  // 139858793432832
   // t5.join();  // terminate called after throwing an instance of 'std::system_error'
   t6.join();
 
@@ -76,8 +72,6 @@ namespace joinable {
 // 有趣的是，detach()可否调用也是使用joinable()来判断。
 
 // 如果使用detach()，就必须保证线程结束之前可访问数据的有效性，使用指针和引用需要格外谨慎。
-// 线程对象和对象内部管理的线程的生命周期并不一样，如果线程执行的快，可能内部的线程已经结束了，但是线程对象还活着，
-// 也有可能线程对象已经被析构了，内部的线程还在运行。
 // 主线程并不想等待子线程结束就想结束整个任务，直接删掉t1.join()是不行的，
 // 程序会被终止（析构t1的时候会调用std::terminate，程序会打印terminate called without an active exception）。
 // 调用t1.detach()，从而将线程放在后台运行，所有权和控制权被转交给C++运行时库，以确保与线程相关联的资源在线程退出后能被正确的回收。
@@ -91,9 +85,27 @@ void func2() {
   std::cout << "func2 end" << std::endl;
 }
 
+void func3() {
+  // 延时500ms为了保证func4或者func5运行结束之后才打印
+  std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  std::cout << "I'm func3" << std::endl;
+}
+void func4() {
+  std::thread t(func3);
+  t.join();
+  std::cout << "func4 join finished" << std::endl;
+}
+void func5() {
+  std::thread t(func3);
+  t.detach();
+  std::cout << "func5 detach finished" << std::endl;
+}
+
 void testJoinable() {
   // 由默认构造函数创建的线程是不能被join的
   std::thread t1;
+  std::cout << "null thread :" << std::boolalpha << t1.joinable()
+            << std::endl;  // false
   // t1.join();  // terminate called after throwing an instance of 'std::system_error'
 
   // 线程已经执行完任务，但是没有被join的话，该线程依然会被认为是一个活动的执行线程，因此也是可以被join的
@@ -119,6 +131,18 @@ void testJoinable() {
             << std::endl;  // false
   // 线程被分离了，就不能够再被join了。如果调用，会崩溃
   // t4.join();  // terminate called after throwing an instance of 'std::system_error'
+
+  // 线程对象和对象内部管理的线程的生命周期并不一样，如果线程执行的快，可能内部的线程已经结束了，但是线程对象还活着，
+  // 也有可能线程对象已经被析构了，内部的线程还在运行。
+  func4();
+  // I'm func3
+  // func4 join finished
+  func5();  // 退出时，局部的线程t被析构，但是它负责的线程还能运行，也会有打印
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  // func5 detach finished
+  // I'm func3
+  func5();  // 没有func3的打印，因为主线程执行的太快，整个程序已经结束了，那个后台线程被C++运行时库回收了
+  // func5 detach finished
 }
 }  // namespace joinable
 
@@ -164,60 +188,93 @@ void testParameter() {
 }
 }  // namespace parameter
 
-int main() {
-  int type = 1;
-  switch (type) {
-    case 0:
-      create::testCreate();
-      break;
-    case 1:
-      joinable::testJoinable();
-      break;
-    case 2:
-      parameter::testParameter();
-      break;
+namespace functype {
+void globleFunc() { std::cout << "globleFunc" << std::endl; }
+
+class ClassFunc {
+ public:
+  void memberFunc() { std::cout << "memberFunc" << std::endl; }
+  static void staticMemberFunc() {
+    std::cout << "staticMemberFunc" << std::endl;
   }
-}
+};
 
-void f2(int &n) {  // 拷贝构造函数
-  for (int i = 0; i < 5; ++i) {
-    std::cout << "Thread 2 executing\n";
-    ++n;
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+void testFuncType() {
+  std::thread t1(globleFunc);  // 全局函数
+  t1.join();
+
+  std::thread t2(ClassFunc::staticMemberFunc);  // 类中的静态成员函数
+  t2.join();
+
+  ClassFunc cf;
+  std::thread t3(std::bind(&ClassFunc::memberFunc, &cf));  // 类的普通成员函数
+  t3.join();
+  std::thread t4(&ClassFunc::memberFunc, &cf);  // 类的普通成员函数
+  t4.join();
+
+  std::thread t5([]() { std::cout << "lamdafunc" << std::endl; });
+  t5.join();
+
+  std::function<void()> f1 = []() { std::cout << "function" << std::endl; };
+  std::thread t6(f1);  // function对象
+  t6.join();
+
+  typedef void (*FF)();
+  FF f2 = globleFunc;
+  std::thread t7(f2);  // 函数指针
+  t7.join();
+}
+}  // namespace functype
+
+namespace classext {
+// 线程功能拓展
+class MyThread : public std::thread {  // 继承thread
+ public:
+  // 子类MyThread()继承thread()的构造函数
+  MyThread() : std::thread() {}
+  // MyThread()初始化构造函数
+  template <typename T, typename... Args>
+  MyThread(T &&func, Args &&... args)
+      : std::thread(std::forward<T>(func), std::forward<Args>(args)...) {}
+  void showcmd(const char *str) { system(str); }
+};
+
+// 在类内部创建线程
+class HelloThread {
+ public:
+  // start()和hellothread()方法都必须是static方法
+  static void hellothread() {
+    std::cout << "hello world, I'm a thread!" << std::endl;
   }
+  static void start() {
+    std::thread thrd(hellothread);
+    thrd.join();
+  }
+};
+
+void testClassext() {
+  MyThread t1([]() { std::cout << "hello" << std::endl; });
+  t1.showcmd("ls");
+  t1.join();
+
+  MyThread t2([](const char *str) { std::cout << "hello" << str << std::endl; },
+              " this is MyThread");
+  t2.showcmd("pwd");
+  t2.join();
+
+  HelloThread::start();
+}
+}  // namespace classext
+
+namespace otherfunc {
+void func1() {
+  // sleep_for：线程休眠某个指定的时间片(time span)，该线程才被重新唤醒
+  // 不过由于线程调度等原因，实际休眠时间可能比sleep_duration所表示的时间片更长
+  std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
-void thread_task(int n) {
-  std::this_thread::sleep_for(std::chrono::seconds(n));
-  std::cout << "hello thread " << std::this_thread::get_id() << " paused " << n
-            << " seconds" << std::endl;
-}
-
-// 其他成员函数:
-// get_id:获取线程ID，返回一个类型为std::thread::id的对象。
-
-// swap:Swap线程，交换两个线程对象所代表的底层句柄(underlying handles)。
-
-// native_handle:返回native handle（由于std::thread的实现和操作系统相关，因此该函数返回与std::thread具体实现相关的线程句柄，例如在符合
-// Posix标准的平台下(如Unix/Linux)是Pthread库）。
-
-// hardware_concurrency [static]: 检测硬件并发特性，返回当前平台的线程实现所支持的线程并发数目，但返回值仅仅只作为系统提示(hint)。
-
-// std::this_thread命名空间中相关辅助函数介绍:
-// get_id:获取线程ID。
-// yield:当前线程放弃执行，操作系统调度另一线程继续执行。
-// sleep_until:线程休眠至某个指定的时刻(time point)，该线程才被重新唤醒。
-// sleep_for:线程休眠某个指定的时间片(time span)，该线程才被重新唤醒，不过由于线程调度等原因，实际休眠时间可能比sleep_duration所表示的时间片更长。
-
-void foo() { std::this_thread::sleep_for(std::chrono::seconds(1)); }
-void bar() { std::this_thread::sleep_for(std::chrono::seconds(1)); }
-void independentThread() {
-  std::cout << "Starting concurrent thread.\n";
-  std::this_thread::sleep_for(std::chrono::seconds(2));
-  std::cout << "Exiting concurrent thread.\n";
-}
 std::mutex iomutex;
-void f(int num) {
+void func2(int num) {
   std::this_thread::sleep_for(std::chrono::seconds(1));
 
   sched_param sch;
@@ -227,14 +284,7 @@ void f(int num) {
   std::cout << "Thread " << num << " is executing at priority "
             << sch.sched_priority << '\n';
 }
-std::mutex g_display_mutex;
-void foo1() {
-  std::thread::id this_id = std::this_thread::get_id();
-  g_display_mutex.lock();
-  std::cout << "thread " << this_id << " sleeping...\n";
-  g_display_mutex.unlock();
-  std::this_thread::sleep_for(std::chrono::seconds(1));
-}
+
 void little_sleep(std::chrono::microseconds us) {
   auto start = std::chrono::high_resolution_clock::now();
   auto end = start + us;
@@ -243,177 +293,36 @@ void little_sleep(std::chrono::microseconds us) {
   } while (std::chrono::high_resolution_clock::now() < end);
 }
 
-void function_1() {
-  // 延时500ms为了保证test()运行结束之后才打印
-  std::this_thread::sleep_for(std::chrono::milliseconds(500));
-  std::cout << "I'm function_1()" << std::endl;
-}
-void test() {
-  std::thread t1(function_1);
-  // t1.detach();
+void testOtherfunc() {
+  // get_id：获取线程ID，返回一个类型为std::thread::id的对象
+  // swap：交换两个线程对象所代表的底层句柄
+  std::thread t1(func1);
+  std::thread t2(func1);
+  std::cout << "thread 1 id: " << t1.get_id() << std::endl;  // 139681204881152
+  std::cout << "thread 2 id: " << t2.get_id() << std::endl;  // 139681196488448
+  std::swap(t1, t2);
+  std::cout << "after std::swap(t1, t2):" << std::endl;
+  std::cout << "thread 1 id: " << t1.get_id() << std::endl;  // 139681196488448
+  std::cout << "thread 2 id: " << t2.get_id() << std::endl;  // 139681204881152
+  t1.swap(t2);
+  std::cout << "after t1.swap(t2):" << std::endl;
+  std::cout << "thread 1 id: " << t1.get_id() << std::endl;  // 139681204881152
+  std::cout << "thread 2 id: " << t2.get_id() << std::endl;  // 139681196488448
   t1.join();
-  std::cout << "test() finished" << std::endl;
-}
-
-// 。
-void test1() {
-  std::thread t1(function_1);
-  t1.detach();  // 脱离主线程的绑定，主线程挂了，子线程不报错，子线程执行完自动退出。detach以后，子线程会成为孤儿线程，线程之间将无法通信。
-  if (t1.joinable()) t1.join();
-  assert(!t1.joinable());
-}
-
-void show() { cout << "hello cplusplus!" << endl; }
-void show1(const char *str, const int id) {
-  cout << "线程 " << id + 1 << " ：" << str << endl;
-}
-// 多线程可变参数
-int show2(const char *fun, ...) {
-  va_list ap;         // 指针
-  va_start(ap, fun);  // 开始
-  vprintf(fun, ap);   // 调用
-  va_end(ap);
-  return 0;
-}
-
-// 线程功能拓展
-class MyThread : public thread  // 继承thread
-{
- public:
-  // 子类MyThread()继承thread()的构造函数
-  MyThread() : thread() {}
-  // MyThread()初始化构造函数
-  template <typename T, typename... Args>
-  MyThread(T &&func, Args &&... args)
-      : thread(forward<T>(func), forward<Args>(args)...) {}
-  void showcmd(const char *str)  // 运行system
-  {
-    system(str);
-  }
-};
-
-// 在类内部创建线程
-class HelloThread {
- public:
-  // start()和hellothread()方法都必须是static方法
-  static void hellothread() { cout << "hello world, I'm a thread!" << endl; }
-  static void start() {
-    thread thrd(hellothread);
-    thrd.join();
-  }
-};
-
-// 用类内部函数在类外部创建线程
-class HelloWorld {
- public:
-  void hello(int year) { cout << "I am " << year << " years old" << endl; }
-};
-
-void proc(int &x) {
-  cout << x << "," << &x << endl;
-  x *= 10;
-}
-
-/*
-int main() {
-  // 构造函数
-  int n = 0;
-  std::thread t1;                   // t1 is not a thread
-  std::thread t2(f1, n + 1);        // pass by value
-  std::thread t3(f2, std::ref(n));  // pass by reference
-  // t4 is now running f2(). t3 is no longer a thread
-  std::thread t4(std::move(t3));
   t2.join();
-  t4.join();
-  std::cout << "Final value of n is " << n << '\n';
 
-  // 赋值操作
-  std::thread threads[2];
-  std::cout << "Spawning 2 threads...\n";
-  for (int i = 0; i < 2; i++) {
-    threads[i] = std::thread(thread_task, i + 1);
-  }
-  std::cout << "Done spawning threads! Now wait for them to join\n";
-  for (auto &t : threads) {
-    t.join();
-  }
-  std::cout << "All threads joined.\n";
-
-  // get_id
-  std::thread t11(foo);
-  std::thread::id t1_id = t11.get_id();
-  std::thread t22(foo);
-  std::thread::id t2_id = t22.get_id();
-  // id每次的值不同
-  std::cout << "t1's id: " << t1_id << '\n';  // 140420714456832
-  std::cout << "t2's id: " << t2_id << '\n';  // 140420722849536
-  t11.join();
-  t22.join();
-
-  // joinable
-  std::thread t33;
-  std::cout << "before starting, joinable: " << t33.joinable() << '\n';  // 0
-  t33 = std::thread(foo);
-  std::cout << "after starting, joinable: " << t33.joinable() << '\n';  // 1
-  t33.join();
-
-  // join
-  std::cout << "starting first helper...\n";
-  std::thread helper1(foo);
-  std::cout << "starting second helper...\n";
-  std::thread helper2(bar);
-  std::cout << "waiting for helpers to finish..." << std::endl;
-  helper1.join();
-  helper2.join();
-  std::cout << "done!\n";
-
-  // detach
-  std::cout << "Starting thread caller.\n";
-  std::thread t44(independentThread);
-  t44.detach();
-  std::this_thread::sleep_for(std::chrono::seconds(1));
-  std::cout << "Exiting thread caller.\n";
-  std::this_thread::sleep_for(std::chrono::seconds(3));
-  // Starting thread caller.
-  // Starting concurrent thread.
-  // Exiting thread caller.
-  // Exiting concurrent thread.
-
-  // swap
-  std::thread tt1(foo);
-  std::thread tt2(bar);
-  std::cout << "thread 1 id: " << tt1.get_id() << std::endl;
-  std::cout << "thread 2 id: " << tt2.get_id() << std::endl;
-  std::swap(tt1, tt2);
-  std::cout << "after std::swap(tt1, tt2):" << std::endl;
-  std::cout << "thread 1 id: " << tt1.get_id() << std::endl;
-  std::cout << "thread 2 id: " << tt2.get_id() << std::endl;
-  tt1.swap(tt2);
-  std::cout << "after tt1.swap(tt2):" << std::endl;
-  std::cout << "thread 1 id: " << tt1.get_id() << std::endl;
-  std::cout << "thread 2 id: " << tt2.get_id() << std::endl;
-  tt1.join();
-  tt2.join();
-  // thread 1 id: 139857369171712
-  // thread 2 id: 139857360779008
-  // after std::swap(tt1, tt2):
-  // thread 1 id: 139857360779008
-  // thread 2 id: 139857369171712
-  // after tt1.swap(tt2):
-  // thread 1 id: 139857369171712
-  // thread 2 id: 139857360779008
-
-  // native_handle
-  std::thread x1(f, 1), x2(f, 2);
+  // native_handle：返回与std::thread具体实现相关的线程句柄
+  // std::thread的实现和操作系统相关，在符合Posix标准的平台下(如Unix/Linux)是Pthread库
+  std::thread t3(func2, 1), t4(func2, 2);
   sched_param sch;
   int policy;
-  pthread_getschedparam(x1.native_handle(), &policy, &sch);
+  pthread_getschedparam(t3.native_handle(), &policy, &sch);  // unix api
   sch.sched_priority = 20;
-  if (pthread_setschedparam(x1.native_handle(), SCHED_FIFO, &sch)) {
+  if (pthread_setschedparam(t3.native_handle(), SCHED_FIFO, &sch)) {
     std::cout << "Failed to setschedparam: " << std::strerror(errno) << '\n';
   }
-  x1.join();
-  x2.join();
+  t3.join();
+  t4.join();
   // Failed to setschedparam: Operation not permitted
   // Thread 1 is executing at priority 0
   // Thread 2 is executing at priority 0
@@ -421,24 +330,12 @@ int main() {
   // Thread 2 is executing at priority 0
   // Thread 1 is executing at priority 20
 
-  // hardware_concurrency
+  // hardware_concurrency [static]:
+  // 检测硬件并发特性，返回当前平台的线程实现所支持的线程并发数目，但返回值仅仅只作为系统提示(hint)。
   unsigned int n1 = std::thread::hardware_concurrency();
-  std::cout << n1 << " concurrent threads are supported.\n";
-  // 8 concurrent threads are supported.
+  std::cout << n1 << " concurrent threads are supported.\n";  // 8
 
-  // this_thread::get_id
-  std::thread y1(foo1);
-  std::thread y2(foo1);
-  std::cout << "thread : " << y1.get_id() << std::endl;
-  std::cout << "thread : " << y2.get_id() << std::endl;
-  y1.join();
-  y2.join();
-  // thread 139677731620608 sleeping...
-  // thread : 139677731620608
-  // thread thread : 139677723227904
-  // 139677723227904 sleeping...
-
-  // this_thread::yield
+  // this_thread::yield，当前线程放弃执行，操作系统调度另一线程继续执行。
   auto start = std::chrono::high_resolution_clock::now();
   little_sleep(std::chrono::microseconds(100));
   auto elapsed = std::chrono::high_resolution_clock::now() - start;
@@ -447,7 +344,7 @@ int main() {
       << std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count()
       << " microseconds\n";
 
-  // this_thread::sleep_until
+  // sleep_until:线程休眠至某个指定的时刻(time point)，该线程才被重新唤醒。
   using std::chrono::system_clock;
   std::time_t tt = system_clock::to_time_t(system_clock::now());
   struct std::tm *ptm = std::localtime(&tt);
@@ -460,90 +357,39 @@ int main() {
   // Current time: 15:44:27
   // Waiting for the next minute to begin...
   // 15:45:00 reached!
-
-  // this_thread::sleep_for
-  std::cout << "Hello waiter" << std::endl;
-  std::chrono::milliseconds dura(2000);
-  std::this_thread::sleep_for(dura);
-  std::cout << "Waited 2000 ms\n";
-
-  test();
-  // 让主线程晚于子线程结束
-  std::this_thread::sleep_for(std::chrono::milliseconds(1000));  // 延时1s
-  // 使用t1.detach()时
-  // test() finished
-  // I'm function_1()
-  // 使用t1.join()时
-  // I'm function_1()
-  // test() finished
-  // 由于线程入口函数内部有个500ms的延时，所以在还没有打印的时候，test()已经执行完成了，t1已经被析构了，但是它负责的那个线程还是能够运行，这就是detach()的作用。
-  // 如果去掉main函数中的1s延时，会发现什么都没有打印，因为主线程执行的太快，整个程序已经结束了，那个后台线程被C++运行时库回收了。
-  // 如果将t1.detach()换成t1.join()，test函数会在t1线程执行结束之后，才会执行结束。
-
-  // 栈上
-  // thread z1(show);  // 根据函数初始化执行
-  // thread z2(show);
-  // thread z3(show);
-  // 线程数组
-  // thread th[3]{thread(show), thread(show), thread(show)};
-  // 堆上
-  // thread *pt1(new thread(show));
-  // thread *pt2(new thread(show));
-  // thread *pt3(new thread(show));
-  // 线程指针数组
-  // thread *pth(new thread[3]{thread(show), thread(show), thread(show)});
-
-  thread v1(show1, "hello cplusplus!", 0);
-  thread v2(show1, "你好，C++！", 1);
-  thread v3(show1, "hello!", 2);
-  v1.join();
-  v2.join();
-  v3.join();
-
-  auto fun = [](const char *str) { cout << str << endl; };
-  thread l1(fun, "hello world!");
-  thread l2(fun, "hello beijing!");
-  l1.join();
-  l2.join();
-
-  // 线程d2可以通过move移动t1来获取d1的全部属性，而d1却销毁了。
-  thread d1([]() { cout << "thread1" << endl; });
-  cout << "thread1' id is " << d1.get_id() << endl;
-  thread d2 = move(d1);
-  cout << "thread2' id is " << d2.get_id() << endl;
-  // d1.join();
-  d2.join();
-
-  thread m1(show2, "%s %d %c %f", "hello world!", 100, 'A', 3.14159);
-  m1.join();
-
-  MyThread c1([]() { cout << "hello" << endl; });
-  c1.showcmd("calc");  // 运行calc
-  MyThread c2([](const char *str) { cout << "hello" << str << endl; },
-              " this is MyThread");
-  c2.showcmd("notepad");  // 运行notepad
-  c1.join();
-  c2.join();
-
-  HelloThread::start();
-
-  HelloWorld obj;
-  thread thrd(std::bind(&HelloWorld::hello, &obj, 26));  // I am 26 years old
-  thrd.join();
-
-  int a = 10;
-  cout << a << "," << &a << endl;
-  thread a1(proc, ref(a));
-  a1.join();
-  cout << a << "," << &a << endl;
-  // 10,0x7ffc49835df4
-  // 10,0x7ffc49835df4
-  // 100,0x7ffc49835df4
-
-  return 0;
 }
-*/
+}  // namespace otherfunc
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    std::cout << argv[0] << " i [0 - 5]" << std::endl;
+    return 0;
+  }
+  int type = argv[1][0] - '0';
+  switch (type) {
+    case 0:
+      create::testCreate();
+      break;
+    case 1:
+      joinable::testJoinable();
+      break;
+    case 2:
+      parameter::testParameter();
+      break;
+    case 3:
+      functype::testFuncType();
+      break;
+    case 4:
+      classext::testClassext();
+      break;
+    case 5:
+      otherfunc::testOtherfunc();
+      break;
+    default:
+      std::cout << "invalid type" << std::endl;
+      break;
+  }
+}
+
 // GCC默认没有加载pthread库，据说在后续的版本中可以不用在编译时添加-pthread选项。
 // g++ -std=c++11 thread.cpp -lpthread
-
-// https://www.bookstack.cn/books/CPP-Concurrency-In-Action-2ed-2019
