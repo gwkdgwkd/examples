9796216ded6f776e646e0d9a6a458745521ebcbc
wlliang <thierryliang@126.com>
2022-03-08 12:05:47 +0800

    update c++ c2c++
    Signed-off-by: wlliang <thierryliang@126.com>

--- c++/c2c++/new_delete.cpp
+++ c++/c2c++/new_delete.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 
-using namespace std;
-
 // 和malloc()一样，new也是在堆区分配内存，必须手动释放，否则只能等到程序运行结束由操作系统回收。
-// 为了避免内存泄露，通常new和delete、new[]和delete[]操作符应该成对出现，并且不要和C语言中malloc()、free()一起混用。
-// 在C++中，建议使用new和delete来管理内存，它们可以使用C++的一些新特性，最明显的是可以自动调用构造函数和析构函数。
+// 为了避免内存泄露，通常new和delete、new[]和delete[]操作符应该成对出现，
+// 并且不要和C语言中malloc()、free()一起混用。
+// 在C++中，建议使用new和delete来管理内存，它们可以使用C++的一些新特性，
+// 最明显的是可以自动调用构造函数和析构函数。
 
 int main() {
   // 在C语言中，动态分配内存用malloc()函数，释放内存用free()函数：
   int *p1 = (int *)malloc(sizeof(int) * 10);
   free(p1);
 
-  // 在C++中，这两个函数仍然可以使用，但是C++又新增了两个关键字，new和delete：new用来动态分配内存，delete用来释放内存。
+  // 在C++中，这两个函数仍然可以使用，但是C++又新增了两个关键字，
+  // new和delete：new用来动态分配内存，delete用来释放内存。
   int *p2 = new int;  // new操作符会根据后面的数据类型来推断所需空间的大小。
   delete p2;
 
