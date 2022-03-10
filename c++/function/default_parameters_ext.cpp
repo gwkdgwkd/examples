695e1651e1e74f3e6771fa022a84ce3dd4546d99
wlliang <thierryliang@126.com>
2022-03-10 08:51:32 +0800

    update c++ default parameters
    Signed-off-by: wlliang <thierryliang@126.com>

--- c++/function/default_parameters_ext.cpp
+++ c++/function/default_parameters_ext.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
-using namespace std;
 
+namespace n2 {
 void func2(int a, int b = 10, int c = 36) {
-  cout << a << ", " << b << ", " << c << endl;
+  std::cout << a << "," << b << "," << c << std::endl;
 }
 
-void func3(int a, int b = 8, int c = 44) {
-  cout << a << ", " << b << ", " << c << endl;
+void func3(int a, int b = 30, int c = 60) {
+  std::cout << a << "," << b << "," << c << std::endl;
 }
+
+void func4() {
+  func3(1);
+  func3(1, 2);
+  func3(1, 2, 3);
+}
+}  // namespace n2
