59153ac09e6e5adb3e7e4433da7b091dfbec295b
wlliang <thierryliang@126.com>
2022-03-31 17:48:31 +0800

    update sort
    Signed-off-by: wlliang <thierryliang@126.com>

--- data_structure/c/sort/merge_sort.c
+++ data_structure/c/sort/merge_sort.c
@@ -1,104 +1,49 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 // 归并排序算法是在分治算法(分而后立)基础上设计出来的一种排序算法，对应的时间复杂度为O(nlogn)。
 // 归并排序算法实现排序的思路是：
-//   将整个待排序序列划分成多个不可再分的子序列，每个子序列中仅有1个元素；
-//   所有的子序列进行两两合并，合并过程中完成排序操作，最终合并得到的新序列就是有序序列。
+// 1.将整个待排序序列划分成多个不可再分的子序列，每个子序列中仅有1个元素；
+// 2.所有的子序列进行两两合并，合并过程中完成排序操作，最终合并得到的新序列就是有序序列。
 
-// 对于含有n个记录的无序表，首先默认表中每个记录各为一个有序表（只不过表的长度都为1），然后进行两两合并，使n个有序表变
-// 为⌈n/2⌉个长度为2或者1的有序表（例如4个小有序表合并为2个大的有序表），通过不断地进行两两合并，直到得到一个长度为n的有序表为止。
+// 对于含有n个记录的无序表，首先默认表中每个记录各为一个有序表（只不过表的长度都为1），然后进行两两合并，
+// 使n个有序表变为⌈n/2⌉个长度为2或者1的有序表（例如4个小有序表合并为2个大的有序表），
+// 通过不断地进行两两合并，直到得到一个长度为n的有序表为止。
 // 这种归并排序方法称为：2-路归并排序。
 
 // 归并过程中，每次得到的新的子表本身有序，所以最终得到的为有序表。
-// 归并排序算法在具体实现时，首先需要将整个记录表进行折半分解，直到分解为一个记录作为单独的一张表为止，然后在进行两两合并。
+// 归并排序算法在具体实现时，首先需要将整个记录表进行折半分解，
+// 直到分解为一个记录作为单独的一张表为止，然后在进行两两合并。
 
-// 该算法相比于堆排序和快速排序，其主要的优点是：当记录表中含有值相同的记录时，排序前和排序后在表中的相对位置不会改变。
+// 该算法相比于堆排序和快速排序，其主要的优点是：
+// 当记录表中含有值相同的记录时，排序前和排序后在表中的相对位置不会改变。
 
-#define MAX 8
-typedef struct {
-  int key;
-} SqNode;
-typedef struct {
-  SqNode r[MAX];
-  int length;
-} SqList;
-// SR中的记录分成两部分：下标从i至m有序，从m+1至n也有序，此函数的功能是合二为一至TR数组中，使整个记录表有序
-void Merge(SqNode SR[], SqNode TR[], int i, int m, int n) {
-  int j, k;
-  // 将SR数组中的两部分记录按照从小到大的顺序添加至TR数组中
-  for (j = m + 1, k = i; i <= m && j <= n; k++) {
-    if (SR[i].key < SR[j].key) {
-      TR[k] = SR[i++];
-    } else {
-      TR[k] = SR[j++];
-    }
-  }
-  // 将剩余的比目前TR数组中都大的记录复制到TR数组的最后位置
-  while (i <= m) {
-    TR[k++] = SR[i++];
-  }
-  while (j <= n) {
-    TR[k++] = SR[j++];
-  }
-}
-void MSort(SqNode SR[], SqNode TR1[], int s, int t) {
-  SqNode TR2[MAX];
-  // 递归的出口
-  if (s == t) {
-    TR1[s] = SR[s];
-  } else {
-    int m = (s + t) / 2;  // 每次递归将记录表中记录平分，直至每个记录各成一张表
-    MSort(SR, TR2, s, m);  // 将分开的前半部分表中的记录进行排序
-    MSort(SR, TR2, m + 1, t);  // 将后半部分表中的记录进行归并排序
-    Merge(TR2, TR1, s, m, t);  // 最后将前半部分和后半部分中的记录统一进行排序
-  }
-}
-// 归并排序
-void MergeSort(SqList *L) { MSort(L->r, L->r, 1, L->length); }
+void Merge(int *nums, int start, int mid, int end) {
+  int llen = mid - start + 1;
+  int rlen = end - mid;
+  int left[llen], right[rlen];
+  memcpy(left, nums + start, sizeof(int) * llen);
+  memcpy(right, nums + mid + 1, sizeof(int) * rlen);
 
-// 实现归并操作的函数，归并的2个区域分别为[p,mid]和[mid+1,q]
-void merge(int *arr, int p, int mid, int q) {
-  int i, j, k;
-  int leftarr[100], rightarr[100];
-  int numL = mid - p + 1;
-  int numR = q - mid;
-  // 将arr数组中[p,mid]区域内的元素逐一拷贝到leftarr数组中
-  for (i = 0; i < numL; i++) {
-    leftarr[i] = arr[p - 1 + i];
+  int li = 0, ri = 0, k = start;
+  while (li < llen && ri < rlen) {
+    nums[k++] = left[li] < right[ri] ? left[li++] : right[ri++];
   }
-  // 将arr数组中[mid+1,q]区域内的元素逐一拷贝到rightarr数组中
-  leftarr[i] = 2147483647;
-  for (i = 0; i < numR; i++) {
-    rightarr[i] = arr[mid + i];
-  }
-  rightarr[i] = 2147483647;
-  i = 0;
-  j = 0;
-  // 逐一比较leftarr和rightarr中的元素，每次将较小的元素拷贝到arr数组中的[p,q]区域内
-  for (k = p; k <= q; k++) {
-    if (leftarr[i] <= rightarr[j]) {
-      arr[k - 1] = leftarr[i];
-      i++;
-    } else {
-      arr[k - 1] = rightarr[j];
-      j++;
-    }
+  while (li < llen) {
+    nums[k++] = left[li++];
   }
 }
-// 实现分割操作的函数，[p,q]用于指定归并排序的区域范围，
-void merge_sort(int *arr, int p, int q) {
-  int mid;
-  if (arr == NULL || p > q || p == q) {
-    return;
+void MergeSort(int *nums, int start, int end) {
+  if (start < end) {
+    int mid = start + (end - start) / 2;
+    // 归
+    MergeSort(nums, start, mid);
+    MergeSort(nums, mid + 1, end);
+    // 并
+    Merge(nums, start, mid, end);
   }
-  mid = (p + q) / 2;
-  // 将[p,q]分为[p,mid]和[mid+1,q]区域
-  merge_sort(arr, p, mid);
-  merge_sort(arr, mid + 1, q);
-  // 对分好的[p,mid]和[mid,q]进行归并操作
-  merge(arr, p, mid, q);
 }
+
 void print(int *list, int size) {
   for (int i = 0; i < size; i++) {
     printf("%d ", list[i]);
@@ -108,23 +53,9 @@ void print(int *list, int size) {
 
 int main() {
   int a[] = {35, 33, 42, 10, 14, 19, 27, 44};
-  merge_sort(a, 1, sizeof(a) / sizeof(int));
-  print(a, sizeof(a) / sizeof(int));  // 10 14 19 27 33 35 42 44
-
-  SqList *L = (SqList *)malloc(sizeof(SqList));
-  L->length = 7;
-  L->r[1].key = 49;
-  L->r[2].key = 38;
-  L->r[3].key = 65;
-  L->r[4].key = 97;
-  L->r[5].key = 76;
-  L->r[6].key = 13;
-  L->r[7].key = 27;
-  MergeSort(L);
-  for (int i = 1; i <= L->length; i++) {
-    printf("%d ", L->r[i].key);
-  }
-  printf("\n");  // 13 27 38 49 65 76 97
+  MergeSort(a, 0, sizeof(a) / sizeof(int) - 1);
+  print(a, sizeof(a) / sizeof(int));
+  // 10 14 19 27 33 35 42 44
 
   return 0;
 }
--- data_structure/c/sort/shell_sort.c
+++ data_structure/c/sort/shell_sort.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 // 希尔排序算法又叫缩小增量排序算法，是一种更高效的插入排序算法。
-// 和普通的插入排序算法相比，希尔排序算法减少了移动元素和比较元素大小的次数，从而提高了排序效率，时间效率上有很大的改进。
+// 和普通的插入排序算法相比，希尔排序算法减少了移动元素和比较元素大小的次数，
+// 从而提高了排序效率，时间效率上有很大的改进。
 
 // 在使用直接插入排序算法时，如果表中的记录只有个别的是无序的，多数保持有序，这种情况下算法的效率也会比较高；
-// 除此之外，如果需要排序的记录总量很少，该算法的效率同样会很高。希尔排序就是从这两点出发对算法进行改进得到的排序算法。
+// 除此之外，如果需要排序的记录总量很少，该算法的效率同样会很高。
+// 希尔排序就是从这两点出发对算法进行改进得到的排序算法。
 // 一般在记录的数量多的情况下，希尔排序的排序效率较直接插入排序高。
 
 // 希尔排序的具体实现思路是：
@@ -16,39 +17,6 @@
 // 经过大量的研究表明，所选取的增量值最好是没有除1之外的公因子，同时整个增量数组中最后一个增量值必须等于1，
 // 因为最后必须对整张表做一次直接插入排序算法。
 
-#define SIZE 15
-typedef struct {
-  int key;  // 关键字的值
-} SLNode;
-typedef struct {
-  SLNode r[SIZE];  // 存储记录的数组
-  int length;      // 记录数组中记录的数量
-} SqList;
-// 希尔排序的实现函数，其中dk表示增值量
-void ShellInsert(SqList *L, int dk) {
-  // 从dk+1个记录起，每间隔dk个记录就调取一个进行直接插入排序算法
-  for (int i = dk + 1; i <= L->length; i++) {
-    // 如果新调取的关键字的值，比子表中最后一个记录的关键字小，说明需要将该值调换位置
-    if (L->r[i].key < L->r[i - dk].key) {
-      int j;
-      // 记录表中，使用位置下标为0的空间存储需要调换位置的记录的值
-      L->r[0] = L->r[i];
-      // 做直接插入排序，如果下标为0的关键字比下标为j的关键字小，则向后一行下标为j的值，为新插入的记录腾出空间。
-      for (j = i - dk; j > 0 && (L->r[0].key < L->r[j].key); j -= dk) {
-        L->r[j + dk] = L->r[j];
-      }
-      // 跳出循环后，将新的记录值插入到腾出的空间中，即完成了一次直接插入排序
-      L->r[j + dk] = L->r[0];
-    }
-  }
-}
-// 希尔排序，通过调用不同的增量值（记录），实现对多个子表分别进行直接插入排序
-void ShellSort(SqList *L, int dlta[], int t) {
-  for (int k = 0; k < t; k++) {
-    ShellInsert(L, dlta[k]);
-  }
-}
-
 void print(int *list, int size, int i) {
   if (i > 0) {
     printf("%d: ", i);
@@ -58,28 +26,28 @@ void print(int *list, int size, int i) {
   }
   printf("\n");
 }
-void ShellSort1(int *list, int size) {
-  int temp, i, j;
 
+void ShellSort(int *nums, int size) {
   // 待排序序列如何进行划分，划分多少次，都会影响到希尔排序算法的执行效率。
   // 希尔排序算法没有固定的划分标准，推荐一种常用的方法：
-  int interval = 1;  // 初始化间隔数为1
-  while (interval < size / 3) {
-    interval = interval * 3 + 1;  // 计算最大间隔
+  int interval = 1;              // 初始化间隔数为1
+  while (interval < size / 3) {  // 计算最大间隔
+    interval = interval * 3 + 1;
   }
+
   // 根据间隔数，不断划分序列，并对各子序列排序
-  while (interval > 0) {
-    for (i = interval; i < size; i++) {  // 对各个子序列做直接插入排序
-      temp = list[i];
-      j = i;
-      while (j > interval - 1 && list[j - interval] >= temp) {
-        list[j] = list[j - interval];
-        j -= interval;
+  while (interval) {
+    for (int i = interval; i < size; ++i) {  // 对各个子序列做直接插入排序
+      if (nums[i] < nums[i - interval]) {
+        int temp = nums[i];
+        int j = i - interval;
+        while (j >= interval - 1 && temp < nums[j]) {  // j>=0也行
+          nums[j + interval] = nums[j];
+          j = j - interval;
+        }
+        nums[j + interval] = temp;
       }
-      if (j != i) {
-        list[j] = temp;
-      }
-      print(list, size, interval);
+      print(nums, size, interval);
     }
     interval = (interval - 1) / 3;  // 计算新的间隔数，继续划分序列
   }
@@ -87,7 +55,7 @@ void ShellSort1(int *list, int size) {
 
 int main(int argc, const char *argv[]) {
   int a[] = {35, 33, 42, 10, 14, 19, 27, 44};
-  ShellSort1(a, sizeof(a) / sizeof(int));
+  ShellSort(a, sizeof(a) / sizeof(int));
   // 4: 14 33 42 10 35 19 27 44
   // 4: 14 19 42 10 35 33 27 44
   // 4: 14 19 27 10 35 33 42 44
@@ -99,28 +67,6 @@ int main(int argc, const char *argv[]) {
   // 1: 10 14 19 27 33 35 42 44
   // 1: 10 14 19 27 33 35 42 44
   // 1: 10 14 19 27 33 35 42 44
-
-  // 用数组来存储增量值，例如5代表每间隔5个记录组成一个子表，1表示每间隔一个，也就是最后一次对整张表的直接插入排序
-  int dlta[3] = {5, 3, 1};
-  SqList *L = (SqList *)malloc(sizeof(SqList));
-  L->r[1].key = 49;
-  L->r[2].key = 38;
-  L->r[3].key = 64;
-  L->r[4].key = 97;
-  L->r[5].key = 76;
-  L->r[6].key = 13;
-  L->r[7].key = 27;
-  L->r[8].key = 49;
-  L->r[9].key = 55;
-  L->r[10].key = 4;
-  L->length = 10;
-  // 调用希尔排序算法
-  ShellSort(L, dlta, 3);
-  // 输出语句
-  for (int i = 1; i <= 10; i++) {
-    printf("%d ", L->r[i].key);
-  }
-  printf("\n");  // 4 13 27 38 49 49 55 64 76 97
 
   return 0;
 }
