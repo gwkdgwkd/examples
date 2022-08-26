3cd5ce05688c9e98a097afddefe6e42bdb8aaab0
wlliang <thierryliang@126.com>
2022-08-26 18:34:21 +0800

    update c
    Signed-off-by: wlliang <thierryliang@126.com>

--- c/multi_file_programming/extern/main.c
+++ c/multi_file_programming/extern/main.c
@@ -1,31 +1,41 @@
 #include <stdio.h>
 
-// 多文件编程就是把多个头文件（.h文件）和源文件（.c文件）组合在一起构成一个程序，这是C语言的重点，也是C语言的难点。
+// 多文件编程就是把多个头文件和源文件组合在一起构成一个程序，
+// 这是C语言的重点，也是C语言的难点。
 // 全局变量的作用范围不是从变量定义处到该文件结束，在其他文件中也有效。
 
 // extern关键字，用来声明一个变量或函数。
-// C语言代码是由上到下依次执行的，不管是变量还是函数，原则上都要先定义再使用，否则就会报错。但在实际开发中，经常会在函数或变量定义之前就使用它们，这个时候就需要提前声明。
-// 所谓声明（Declaration），就是告诉编译器我要使用这个变量或函数，你现在没有找到它的定义不要紧，请不要报错，稍后我会把定义补上。
-// 1) 函数的声明
-//    函数的定义有函数体，函数的声明没有函数体，编译器很容易区分定义和声明，所以对于函数声明来说，有没有extern都是一样的。
-//    总结起来，函数声明有四种形式：
-//     datatype function( datatype1 name1, datatype2 name2, ... );
-//     datatype function( datatype1, datatype2, ... );
-//     extern datatype function( datatype1 name1, datatype2 name2, ... );
-//     extern datatype function( datatype1, datatype2, ... );
-// 2) 变量的声明
-//    变量和函数不同，编译器只能根据extern来区分，有extern才是声明，没有extern就是定义。
-//    变量的定义有两种形式，你可以在定义的同时初始化，也可以不初始化：
-//     datatype name = value;
-//     datatype name;
-//    而变量的声明只有一种形式，就是使用 extern 关键字：
-//     extern datatype name;
-//    另外，变量也可以在声明的同时初始化，格式为：
-//     extern datatype name = value;
-//    这种似是而非的方式是不被推荐的，有的编译器也会给出警告，尽量不要这样写。
-// extern是“外部”的意思，很多教材讲到，extern用来声明一个外部（其他文件中）的变量或函数，也就是说，变量或函数的定义在其他文件中。
+// C语言代码是由上到下依次执行的，不管是变量还是函数，
+// 原则上都要先定义再使用，否则就会报错。
+// 但在实际开发中，经常会在函数或变量定义之前就使用它们，这时就需要提前声明。
+// 所谓声明（Declaration），就是告诉编译器我要使用这个变量或函数，
+// 你现在没有找到它的定义不要紧，请不要报错，稍后我会把定义补上。
+// 1.函数的声明
+//   函数的定义有函数体，函数的声明没有函数体，编译器很容易区分定义和声明，
+//   所以对于函数声明来说，有没有extern都是一样的。
+//   总结起来，函数声明有四种形式：
+//   datatype function(datatype1 name1, datatype2 name2, ...);
+//   datatype function(datatype1, datatype2, ...);
+//   extern datatype function(datatype1 name1, datatype2 name2, ...);
+//   extern datatype function(datatype1, datatype2, ...);
+// 2.变量的声明
+//   变量和函数不同，编译器只能根据extern来区分，
+//   有extern才是声明，没有extern就是定义。
+//   变量的定义有两种形式，你可以在定义的同时初始化，也可以不初始化：
+//   datatype name = value;
+//   datatype name;
+//   而变量的声明只有一种形式，就是使用extern关键字：
+//   extern datatype name;
+//   另外，变量也可以在声明的同时初始化，格式为：
+//   extern datatype name = value;
+//   这种似是而非的方式是不被推荐的，有的编译器也会给出警告，尽量不要这样写。
+// extern是外部的意思，很多教材讲到，
+// extern用来声明一个外部（其他文件中）的变量或函数，
+// 也就是说，变量或函数的定义在其他文件中。
 // 不过这样讲不妥，因为除了定义在外部，定义在当前文件中也是正确的。
-// 例如，将module.c中的intm=100;移动到main.c中的任意位置都是可以的。所以我认为，extern是用来声明的，不管具体的定义是在当前文件内部还是外部，都是正确的。
+// 例如，将module.c中的intm=100;移动到main.c中的任意位置都是可以的。
+// 所以我认为，extern是用来声明的，
+// 不管具体的定义是在当前文件内部还是外部，都是正确的。
 
 extern void func();
 extern int m;
--- c/start/typedef.c
+++ c/start/typedef.c
@@ -1,40 +1,48 @@
 #include <stdio.h>
 
-// C语言允许为一个数据类型起一个新的别名，就像给人起“绰号”一样。起别名的目的不是为了提高程序运行效率，而是为了编码方便。
+// C语言允许为一个数据类型起一个新的别名，就像给人起绰号一样。
+// 起别名的目的不是为了提高程序运行效率，而是为了编码方便。
 // typedef的用法一般为：typedef oldName newName;
-// 需要强调的是，typedef是赋予现有类型一个新的名字，而不是创建新的类型。为了“见名知意”，请尽量使用含义明确的标识符，并且尽量大写。
-// typedef还可以给数组、指针、结构体等类型定义别名。
-//  typedef char ARRAY20[20];
-//  char a1[20], a2[20], s1[20], s2[20];
-//  typedef struct stu {
-//    char name[20];
-//    int age;
-//    char sex;
-//  } STU;
-//  STU body1, body2;
-//  struct stu body3, body4;
-//  typedef int (*PTR_TO_ARR)[4];
-//  PTR_TO_ARR p1, p2;
-//  typedef int (*PTR_TO_FUNC)(int, int);
-//  PTR_TO_FUNC pfunc;
+// 需要强调的是，typedef是赋予现有类型一个新的名字，而不是创建新的类型。
+// 为了见名知意，请尽量使用含义明确的标识符，并且尽量大写。
+
+// typedef还可以给数组、指针、结构体等类型定义别名：
+// typedef char ARRAY20[20];
+// char a1[20], a2[20], s1[20], s2[20];
+// typedef struct stu {
+//   char name[20];
+//   int age;
+//   char sex;
+// } STU;
+// STU body1, body2;
+// struct stu body3, body4;
+// typedef int (*PTR_TO_ARR)[4];
+// PTR_TO_ARR p1, p2;
+// typedef int (*PTR_TO_FUNC)(int, int);
+// PTR_TO_FUNC pfunc;
 
 // typedef和#define的区别
-// typedef在表现上有时候类似于#define，但它和宏替换之间存在一个关键性的区别。正确思考这个问题的方法就是把typedef看成一种彻底的“封装”类型，声明之后不能再往里面增加别的东西。
-// 1) 可以使用其他类型说明符对宏类型名进行扩展，但对typedef所定义的类型名却不能这样做。如下所示：
-//  #define INTERGE int
-//  unsigned INTERGE n;  // 没问题
-//  typedef int INTERGE;
-//  unsigned INTERGE n;  // 错误，不能在INTERGE前面添加unsigned
-// 2) 在连续定义几个变量的时候，typedef能够保证定义的所有变量均为同一类型，而#define则无法保证。例如：
-//  #define PTR_INT int *
-//  PTR_INT p1, p2;
-//  经过宏替换以后，第二行变为：
-//  int *p1, p2;
-//  这使得p1、p2成为不同的类型：p1是指向int类型的指针，p2是int类型。
-//  相反，在下面的代码中：
-//  typedef int * PTR_INT
-//  PTR_INT p1, p2;
-//  p1、p2类型相同，它们都是指向int类型的指针。
+// typedef在表现上有时候类似于#define，
+// 但它和宏替换之间存在一个关键性的区别。
+// 正确思考这个问题的方法就是把typedef看成一种彻底的封装类型，
+// 声明之后不能再往里面增加别的东西。
+// 1.可以使用其他类型说明符对宏类型名进行扩展，
+//   但对typedef所定义的类型名却不能这样做。如下所示：
+//   #define INTERGE int
+//   unsigned INTERGE n;  // 没问题
+//   typedef int INTERGE;
+//   unsigned INTERGE n;  // 错误，不能在INTERGE前面添加unsigned
+// 2.在连续定义几个变量的时候，typedef能够保证定义的所有变量均为同一类型，
+//   而#define则无法保证。例如：
+//   #define PTR_INT int *
+//   PTR_INT p1, p2;
+//   经过宏替换以后，第二行变为：
+//   int *p1, p2;
+//   这使得p1、p2成为不同的类型：p1是指向int类型的指针，p2是int类型。
+//   相反，在下面的代码中：
+//   typedef int * PTR_INT
+//   PTR_INT p1, p2;
+//   p1、p2类型相同，它们都是指向int类型的指针。
 
 typedef char (*PTR_TO_ARR)[30];
 typedef int (*PTR_TO_FUNC)(int, int);
